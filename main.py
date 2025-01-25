4788f13235be323abecb96fb3b3f6657abffaa13
matth <matthew.c.hirstius@gmail.com>
2025-01-25 13:25:51 -0600

    logging

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ Og_url = "https://mycscgo.com/laundry/summary/b94db20b-3bf8-4517-9cae-da46d7dd73
 Tr_url = "https://mycscgo.com/laundry/summary/b94db20b-3bf8-4517-9cae-da46d7dd73f6/2303113-026"
 
 try:
+    print("start driver")
     # Navigate to the page
-    print("starting scraping...")
     driver.get(Og_url)
     
     # Optionally, wait a few seconds to allow JavaScript to load content (a more robust approach would be using WebDriverWait)
@@ -31,7 +31,7 @@ try:
     # Locate the elements by XPath
     washers = '//*[@id="root"]/div/main/div[2]/div/div/div[2]/div[1]/p'
     dryers = '//*[@id="root"]/div/main/div[2]/div/div/div[4]/div[1]/p'
-    
+    print("starting scraping")
     try:
         Og_washers = driver.find_element(By.XPATH, washers).text.split('/')[0].strip()
     except Exception as e:
